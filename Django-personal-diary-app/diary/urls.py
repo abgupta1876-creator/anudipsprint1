3fc30d5cd749d8fc6ba60270002dc147e13e7654
abgupta1876-creator <abgupta1876@gmail.com>
2026-03-17 16:39:49 +0530

    Add files via upload

---
+++ Django-personal-diary-app/diary/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('add-entry/', views.add_entry_view, name='add_entry'),
+    path('delete-entry/<int:entry_id>/', views.delete_entry_view, name='delete_entry'),
+]
